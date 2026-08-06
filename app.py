f92645021217306883956c652bb226bdee48813d
Vaishali Patel <vaishalipatel0211@gmail.com>
2026-08-06 10:29:37 +0530

    update port

--- app.py
+++ app.py
@@ -477,5 +477,8 @@ def export_csv():
         mimetype="text/csv",
         headers={"Content-disposition": "attachment; filename=tasks_export.csv"}
     )
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
